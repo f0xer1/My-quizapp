484fc1023af2d24ee9008826975e79c02619e660
Pavlo Ilnitskyi <inavlo092@gmail.com>
2023-10-11 17:24:41 +0300

    Fix question id

--- src/main/java/com/foxer/quizapp/Question.java
+++ src/main/java/com/foxer/quizapp/Question.java
@@ -10,7 +10,7 @@ import lombok.Data;
 @Data
 public class Question {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  int id;
 
     private  String questionTitle;
